d0e954553d3c73ec09a21347dca61c4660dfba52
arimagnolia <89671864+arimagnolia@users.noreply.github.com>
2023-02-20 01:15:13 -0500

    Add files via upload

---
+++ src/Components/HeroImageEx.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import "../styles/HeroImageExp.css";
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import { MdSchool, MdWork } from "react-icons/md";
+
+function HeroImageEx() {
+  return (
+    <div className="hero-exp">
+      <div className='title-exp'>
+        <h2>Experience.</h2>
+      </div>
+        <div className="subtitle-exp">
+          <p>My work and educational background.</p>
+        </div>
+      <VerticalTimeline className='timeline'>
+      <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          date="2022"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<MdWork />}
+          >
+            <h3 className="vertical-timeline-element-title">
+              Application Developer Intern
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle" style={{color:"#fff"}}>
+              Federated Hermes
+            </h4>
+            <p style={{color:"#fff"}}>
+              Assisted senior developers with programming, testing, and documentation of application changes.
+              Promoted code to development, staging, and production environments.
+            </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--education"
+        contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        date="2020 - 2023"
+        iconStyle={{ background: "#3e497a", color: "#fff" }}
+        icon={<MdSchool />}>
+
+        <h3 className="vertical-timeline-element-title">
+          University of Pittsburgh
+        </h3>
+        <h4 className="vertical-timeline-element-subtitle" style={{color:"#fff"}}>
+          Pittsburgh
+        </h4>
+        <p style={{color:"#fff"}}>
+          BS, Computer Science
+        </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            date="2021 - 2022"
+            iconStyle={{ background: "#e9d35b", color: "#fff" }}
+            icon={<MdWork />}
+          >
+            <h3 className="vertical-timeline-element-title" style={{color:"#fff"}}>
+              IT/Office Assistant
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle" style={{color:"#fff"}}>
+              University of Pittsburgh
+            </h4>
+            <p style={{color:"#fff"}}>
+              Provided technical assistance to all departments within the Office of the Provost.
+            </p>
+          </VerticalTimelineElement>
+      <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            date="2017 - 2019"
+            iconStyle={{ background: "#3e497a", color: "#fff" }}
+            icon={<MdSchool />}
+          >
+            <h3 className="vertical-timeline-element-title">
+              Community College of Allegheny County
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle" style={{color:"#fff"}}>
+              Pittsburgh, PA
+            </h4>
+            <p style={{color:"#fff"}}> AS, Mathematics and Sciences</p>
+          </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  )
+}
+
+export default HeroImageEx
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";   
+import HeroImage from "../Components/HeroImage";
+
+function Home() {
+    return (
+      <div className="home">
+        <HeroImage />
+      </div>
+    )
+}
+
+
+export default Home;
