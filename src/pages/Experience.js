07691281aa38d25f380b4ac72fb3d486426b8945
arimagnolia <89671864+arimagnolia@users.noreply.github.com>
2023-03-03 17:30:23 -0500

    Add files via upload

---
+++ src/pages/Experience.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeroImageExp from "../Components/HeroImageEx";
+
+function Experience() {
+  return (
+    <div>
+       <HeroImageExp />
+    </div>
+  )
+}
+
+export default Experience
